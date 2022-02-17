018a99c03d91c6224a224b3762cd83835363c4bd
Guanghui Qin <hiaoxui@gmail.com>
2022-02-17 13:14:40 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='notifier',
+    version='',
+    packages=['notifier'],
+    url='',
+    license='',
+    author='hiaoxui',
+    author_email='hiaoxui@gmail.com',
+    description='',
+    scripts=['notifier/notifier']
+)
