d3d3121a131e15267b4e675ad97f4a467cc52cdc
Guanghui Qin <hiaoxui@gmail.com>
2022-02-17 13:08:25 -0500

    telegram basic functions

---
+++ notifier/bot.py
@@ -0,0 +1,79 @@
+from collections import defaultdict
+import time
+import logging
+from threading import Thread
+
+from telegram.ext import Updater, CallbackContext, CommandHandler
+from telegram import Update, Bot
+
+from notifier.util import load_config
+from notifier.db import Storage
+
+logger = logging.getLogger('notifier')
+
+
+class TelegramBot:
+    def __init__(self, cfg_path=None):
+        self.cfg = load_config(cfg_path)
+        self.db = Storage(**self.cfg['db'])
+        self.bot = Bot(self.cfg['telegram']['token'])
+        self.updater = Updater(token=self.cfg['telegram']['token'], use_context=True)
+        link_handler = CommandHandler('link', self.link)
+        self.updater.dispatcher.add_handler(link_handler)
+        pull_handler = CommandHandler('pull', self.pull)
+        self.updater.dispatcher.add_handler(pull_handler)
+        self.message_count = defaultdict(int)
+        self.last_send = 0
+
+    def link(self, update: Update, context: CallbackContext):
+        args = context.args
+        chat_id = update.effective_chat.id
+        if len(args) < 2 or args[1] != self.cfg['telegram']['secret']:
+            context.bot.send_message(chat_id=chat_id, text='Error')
+            return
+        self.db.link_chat(args[0], chat_id)
+        context.bot.send_message(chat_id=chat_id, text='Received')
+
+    def pull(self, update: Update, context: CallbackContext):
+        logger.warning('Pulling')
+        args = context.args
+        chat_id = update.effective_chat.id
+        past = 3600
+        if len(args) == 1:
+            past = args[1]
+        domains = self.db.cid2domain(chat_id)
+        for domain in domains:
+            self.log(domain, chat_id, 10, past, True)
+        logger.warning('Pulling done')
+
+    def run(self):
+        all_tg = self.db.fetch_tg()
+        for cid, domain, report_level, _ in all_tg:
+            t = Thread(target=self.cron, args=(domain, cid, report_level))
+            t.start()
+        self.updater.start_polling()
+
+    def log(self, domain, cid, level, past, repeat=False):
+        messages = self.db.pull(domain, level, past)
+        for mid, content in messages:
+            if self.message_count[(cid, mid)] > 0 and not repeat:
+                continue
+            self.bot.send_message(text=content, chat_id=cid)
+            self.message_count[(cid, mid)] += 1
+            time.sleep(max(0., self.last_send + 1.0 - time.time()))
+            self.last_send = time.time()
+
+    def cron(self, domain, cid, level):
+        freq = self.cfg['telegram'].get('freq', 30)
+        while True:
+            try:
+                self.log(domain, cid, level, freq*2, False)
+                time.sleep(freq)
+            except Exception as e:
+                logger.error(e)
+            time.sleep(freq)
+
+
+if __name__ == '__main__':
+    tb = TelegramBot()
+    tb.run()
--- notifier/db.py
+++ notifier/db.py
@@ -10,20 +10,68 @@ class Storage:
     ):
         self.conn_args = {'host': host, 'user': user, 'password': password, 'database': database, 'port': port}
 
+    @staticmethod
+    def domain_id(domain, cursor):
+        cursor.execute('SELECT `id` from `domain` where `name` = %s', domain)
+        q_rst = cursor.fetchall()
+        if len(q_rst) == 0:
+            cursor.execute('INSERT INTO `domain` (`name`) VALUES (%s)', domain)
+            cursor.execute('SELECT `id` from `domain` where `name` = %s', domain)
+            q_rst = cursor.fetchall()
+        domain_id = q_rst[0][0]
+        return domain_id
+
     def post(self, level, domain, message):
         conn = pymysql.connect(**self.conn_args)
         cur = conn.cursor()
-        cur.execute('SELECT `id` from `domain` where `name` = %s', domain)
-        q_rst = cur.fetchall()
-        if len(q_rst) == 0:
-            cur.execute('INSERT INTO `domain` (`name`) VALUES (%s)', domain)
-            cur.execute('SELECT `id` from `domain` where `name` = %s', domain)
-            q_rst = cur.fetchall()
-        domain_id = q_rst[0][0]
+        if isinstance(domain, str):
+            domain = self.domain_id(domain, cur)
         cur.execute(
             'INSERT INTO `message` (`level`, `ts`, `content`, `domain`) VALUES (%s, %s, %s, %s)',
-            (level, int(time.time()), message, domain_id)
+            (level, int(time.time()), message, domain)
         )
         cur.close()
         conn.commit()
         conn.close()
+
+    def link_chat(self, domain, chat_id):
+        conn = pymysql.connect(**self.conn_args)
+        cur = conn.cursor()
+        if isinstance(domain, str):
+            domain = self.domain_id(domain, cur)
+        cur.execute('INSERT INTO `tg` (`domain`, `cid`) VALUES (%s, %s)', (domain, chat_id))
+        cur.close()
+        conn.commit()
+        conn.close()
+
+    def pull(self, domain, level, past):
+        conn = pymysql.connect(**self.conn_args)
+        cur = conn.cursor()
+        if isinstance(domain, str):
+            domain = self.domain_id(domain, cur)
+        cur.execute(
+            'SELECT `id`, `content` FROM `message` WHERE domain=%s AND level >= %s AND ts >= %s',
+            (domain, level, int(time.time() - past))
+        )
+        rst = cur.fetchall()
+        cur.close()
+        conn.close()
+        return rst
+
+    def fetch_tg(self):
+        conn = pymysql.connect(**self.conn_args)
+        cur = conn.cursor()
+        cur.execute('SELECT `cid`, `domain`, `report_level`, `base_level` FROM `tg`')
+        rst = cur.fetchall()
+        cur.close()
+        conn.close()
+        return rst
+
+    def cid2domain(self, cid):
+        conn = pymysql.connect(**self.conn_args)
+        cur = conn.cursor()
+        cur.execute('SELECT `domain` FROM `tg` WHERE `cid` = %s', cid)
+        rst = cur.fetchall()
+        cur.close()
+        conn.close()
+        return [item[0] for item in rst]
